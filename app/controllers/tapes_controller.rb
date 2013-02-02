0c60f85fa35e3b7d812cf47f996f3f4eb6ef07ea
Joachim <joachim@nolten.org>
2013-02-02 17:07:41 +0100

    Tapes Tableview works

--- app/controllers/tapes_controller.rb
+++ app/controllers/tapes_controller.rb
@@ -1,9 +1,27 @@
 class TapesController < UIViewController
+  attr_accessor :tapes
+
   def viewDidLoad
     super
     self.title = "Tapes"
     @table = UITableView.alloc.initWithFrame(self.view.bounds)
     @table.autoresizingMask = UIViewAutoresizingFlexibleHeight
     self.view.addSubview(@table)
+    @table.dataSource = self
+    #@tapes = ['tape 1', 'tape 2', 'tape 3']
   end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    @tapes ? @tapes.count : 0
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    @reuseIdentifier ||= "CELL_IDENTIFIER"
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier)
+    cell ||= UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier: @reuseIdentifier)
+    
+    cell.textLabel.text = @tapes[indexPath.row]
+    cell
+  end
+
 end
--- spec/tapes_controller_spec.rb
+++ spec/tapes_controller_spec.rb
@@ -1,15 +1,27 @@
 describe "Tapes Controller" do
+  before do
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.rootViewController = TapesController.alloc.initWithNibName(nil, bundle:nil)
+    @window.rootViewController.tapes = ['tape 1', 'tape 2', 'tape 3']
+    @window.makeKeyAndVisible
+  end
+  
   tests TapesController
 
-  before do
-    @app = UIApplication.sharedApplication
-  end
+  #before do
+    #@app = UIApplication.sharedApplication
+  #end
 
   it "should have title 'Tapes'" do
-    controller.title.should == "Tapes"
+     @window.rootViewController.title.should == "Tapes"
   end
 
   it "should have one UITableView" do
     views(UITableView).count.should == 1
+  end
+
+  it "has one cell for every tape" do
+    views(UITableViewCell).count.should == 3
+    view('tape 1').should.not == nil
   end
 end
