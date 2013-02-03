86fa7ed0efab2c706125ceff5fe16982cadf8e55
Dimitri Tischenko <dimitri@proteon.nl>
2013-02-03 15:14:57 +0100

    first working parser

--- Rakefile
+++ Rakefile
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 $:.unshift("/Library/RubyMotion/lib")
 require 'motion/project'
-#require 'motion-cocoapods'
+require 'motion-cocoapods'
 require 'wakizashi'
 require 'bubble-wrap'
 
 Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
+  app.name = 'reader'
 
   # Only needed if you have not already specifying a pods dependency
   app.pods do
-    dependency 'GDataXML-HTML'
+    pod 'GDataXML-HTML'
   end
 
 end
--- spec/tape_fetcher_spec.rb
+++ spec/tape_fetcher_spec.rb
@@ -4,19 +4,25 @@ describe "TapeFetcher" do
   end
 
   it "has read the html successfully" do
-  	@fetcher.fetch_html
-    @fetcher.html.should.not.equal nil
+  	html = @fetcher.fetch_html
+    html.should.not.equal nil
   end
 
-  it "has created a list of tapes" do
+  it "has created a list of tapes from local html" do
   	tape1 = Tape.new 
   	tape1.name = "Roy B."
   	tape_list = [tape1]
   	html_path = File.expand_path("../tape_fetcher_test.html", __FILE__)
   	# puts "html_path: #{html_path}"
   	html = File.read(html_path)
-  	html_data = html.dataUsingEncoding(NSUTF8StringEncoding)
-	tapes = @fetcher.convert_html_to_tapes html_data 
-	tapes.should.equal tape_list
+  	# html_data = html.dataUsingEncoding(NSUTF8StringEncoding)
+	  tapes = @fetcher.convert_html_to_tapes html 
+	  tapes.size.should.equal 3
+  end
+
+  it "has created a list of tapes from read html" do
+    html = @fetcher.fetch_html
+    tapes = @fetcher.convert_html_to_tapes html
+    tapes.size.should.equal 20
   end
 end
