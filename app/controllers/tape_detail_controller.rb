21439917a44dbe8eee8ac8380f854c42ac2cb4d4
Joachim Nolten <joachim@viewbook.com>
2013-04-17 12:48:27 +0200

    Fixed a problem with Tape url not getting set

--- app/controllers/tape_detail_controller.rb
+++ app/controllers/tape_detail_controller.rb
@@ -21,10 +21,16 @@ class TapeDetailController < UIViewController
     @label.sizeToFit
     @label.center = [self.view.frame.size.width / 2,
       self.view.frame.size.height / 2]
-
-    BW::Media.play(@tape.url) do |media_player|
-      media_player.view.frame = [[10, 100], [100, 100]]
-      self.view.addSubview media_player.view
+    puts @tape.url
+    url = NSURL.URLWithString(@tape.url)
+    error_ptr = Pointer.new(:object)
+    audioPlayer = AVAudioPlayer.alloc.initWithContentsOfURL(url, error: error_ptr)
+    if(audioPlayer == nil)
+      error = error_ptr[0]
+      $stderr.puts "Error playing audio: #{error}"
+    else
+      audioPlayer.numberOfLoops = -1
+      audioPlayer.play
     end
   end
 end
